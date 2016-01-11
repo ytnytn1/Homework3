82af89623a21c9f4155d5a45f1b745f38120fa38
ytnytn1 <ytnytn1@gmail.com>
2016-01-11 20:07:43 +0700

    исправил

---
+++ Homework3/Model/Randomizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public abstract class Randomizer
+    {
+        private static Random _rnd { get; set; }
+        
+        public static Random Rnd
+        {
+            get
+            {
+                if (_rnd == null)
+                {
+                    _rnd = new Random();
+                    return _rnd;
+                }
+                else
+                {
+                    return _rnd;
+                }
+                
+            }
+        }
+    }
+}
